09629bc40592740a84bfb8573bb8bcaf73959326
coderamitdey <amitdey7150@gmail.com>
2025-10-04 15:15:23 +0600

    first commit

---
+++ select-your-eleven/src/App.jsx
@@ -0,0 +1,31 @@
+import { Suspense } from 'react'
+import './App.css'
+import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
+import Navbar from './components/Navbar/Navbar'
+import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
+
+const fetchPlayers = async () => {
+  const res = await fetch("/Players.json");
+  return res.json()
+}
+
+function App() {
+
+  const playersPromise = fetchPlayers()
+
+  return (
+    <>
+      <Navbar></Navbar>
+
+      
+
+      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+        <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
+      </Suspense>
+
+      {/* <SelectedPlayers></SelectedPlayers> */}
+    </>
+  )
+}
+
+export default App
